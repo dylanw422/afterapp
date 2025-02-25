eb3fb1482ddbc2396f35d539afa2375c421d87fb
reactafter <afterframework@gmail.com>
2025-02-25 14:40:13 -0600

    dropped server changes for prod env

--- src/server/index.ts
+++ src/server/index.ts
@@ -33,12 +33,6 @@ const loadApiRoutes = async () => {
   }
 };
 
-app.use(express.static(path.join(__dirname, "../../dist")));
-
-app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "../../dist/index.html"));
-});
-
 const startServer = async () => {
   try {
     loadApiRoutes();
